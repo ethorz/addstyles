0549c1dfec771e95b751ee5a377f101f9e1aff2c
Igor Maslov <ethorz.ru@gmail.com>
2018-03-31 15:43:32 +0300

    Add module file

---
+++ README.md
@@ -0,0 +1,49 @@
+# addstyles [![unstable](http://badges.github.io/stability-badges/dist/unstable.svg)](http://github.com/badges/stability-badges)
+
+> Insert CSS styles into an HTML document and register new font-face for the current web page by URL.
+
+Module use pure javascript code without dependents.
+
+## Usage
+```js
+import addStyles from 'addstyles'; // or import {insertStyles} from 'addstyles';
+
+addStyles.insertStyles('h1 { font-size: 14px }')
+//=> <head><style> h1 { ... }</style></head>
+```
+
+For add font-face to your page:
+```js
+import addStyles from 'addstyles'; // or import {addFont} from 'addstyles';
+
+//Font URL w/o extension inserts eot, woff2, woff, ttf, svg and otf versions
+addStyles.addFont('//cdn.jsdelivr.net/font-hack/2.020/fonts/eot/latin/hack-regular-latin-webfont',
+                  `font-family: Hack; font-weight: normal;`)
+
+//Font URL with extension inserts only target font file
+addStyles.addFont('./wavefont.otf', 'wavefont');
+```
+
+## API
+
+#### `insertStyles(styles, [options]) -> void`
+
+#### addFont(fontUrl, cssString|fontName)
+#### addFont(fontUrlList, cssString|fontName)
+
+Attach font to the page, apply additional parameters, which are whether font name or @font-face properties, eg font-family: <x>; font-style: <y>; font-weight: <z>.
+##### styles
+
+Required. The string of styles to insert into the DOM.
+
+##### options
+
+###### id
+
+Calling `insertStyles` with the same `options.id` multiple times will re-use the same `<style>` element each time.
+
+## Related
+
+* [insert-css](https://github.com/substack/insert-css)
+* [google-fonts](https://github.com/hughsk/google-fonts) — easy-peasy google fonts by name.
+* [webfontloader](https://github.com/typekit/webfontloader) — solution for all possible font include cases.
---
+++ index.js
@@ -0,0 +1,108 @@
+const cache = {};
+
+export default {
+    addFont(src, styles) {
+        let extensions = ['eot', 'woff', 'woff2', 'ttf', 'svg', 'otf'];
+        let format = {
+            eot: 'embedded-opentype',
+            woff2: 'woff2',
+            woff: 'woff',
+            ttf: 'truetype',
+            svg: 'svg',
+            otf: 'opentype'
+        };
+
+        function getFamily(cssString) {
+            if (!/:/.test(cssString)) return cssString;
+            let res = /font(?:-family)?\s*:\s*"?([^;,"']*)"?/ig.exec(cssString);
+            if (!res || !res[1]) return cssString;
+            return res[1];
+        }
+
+        function getStyle(cssString) {
+            if (!/:/.test(cssString)) return 'normal';
+            let res = /font-style\s*:\s*"?([^;,"']*)"?/ig.exec(cssString);
+            if (!res || !res[1]) return 'normal';
+            return res[1];
+        }
+
+        function getWeight(cssString) {
+            if (!/:/.test(cssString)) return 'normal';
+            let res = /font-weight\s*:\s*"?([^;,"']*)"?/ig.exec(cssString);
+            if (!res || !res[1]) return 'normal';
+            return res[1];
+        }
+
+        function extname(str) {
+            const slug = str.split(/\/|\\/).slice(-1)[0];
+            const idx = slug.lastIndexOf('.');
+            if (idx <= 0) return '';
+
+            return slug.slice(idx);
+        }
+
+        let name = getFamily(styles);
+        let ext = extname(src);
+        let urls = [];
+
+        if (Array.isArray(src)) {
+            urls = src;
+        }
+
+        if (!format[ext.slice(1)]) {
+            for (let i = 0; i < extensions.length; i++) {
+                urls.push(src + '.' + extensions[i]);
+            }
+        }
+        else {
+            urls.push(src);
+        }
+
+        let srcStr = 'src: ';
+        for (let i = 0; i < urls.length; i++) {
+            let url = urls[i];
+            let ext = extname(url);
+            let filepath = url.slice(0, -ext.length);
+            if (ext === '.eot') {
+                srcStr += `url("${filepath + '.eot'}");\n`;
+                srcStr += `src: url("${filepath + '.eot?#iefix'}") format("${format.eot}"),\n`;
+            } else {
+                srcStr += `url("${filepath + ext}") format("${format[ext.slice(1)]}"),\n`;
+            }
+        }
+        srcStr = srcStr.trim().slice(0, -1);
+
+        let id = [name, getStyle(styles), getWeight(styles)].join('-');
+
+        this.insertStyles(`
+            @font-face {
+                font-family: "${name}";
+                ${srcStr};
+                ${styles}
+            }
+        `, {id: id});
+    },
+    insertStyles(styles, options) {
+        function createStyle(id) {
+            let element = document.getElementById(id);
+
+            if (element) return element;
+
+            element = document.createElement('style');
+            element.setAttribute('type', 'text/css');
+
+            document.head.appendChild(element);
+
+            return element
+        }
+
+        const id = (options && options.id) || styles;
+        const element = cache[id] = (cache[id] || createStyle(id));
+
+        if ('textContent' in element) {
+            element.textContent = styles
+        } else {
+            element.styleSheet.cssText = styles
+        }
+    }
+}
