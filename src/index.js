45f655007cea461c28aafaa1cfa1255c5f7dd725
Igor Maslov <ethorz.ru@gmail.com>
2018-03-31 22:50:10 +0300

    Add example.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import {addFont, insertStyles} from "./addstyles";
+
 console.log('run example');
+
+const fontPath = './example-font.otf';
+const target = document.getElementById('btn');
+
+target.addEventListener('click', () => {
+    addFont(fontPath, 'example-font');
+    insertStyles('h1 { font-family: "example-font" }');
+});
